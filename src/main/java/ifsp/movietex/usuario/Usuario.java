30bd6e584f65fb2893db8ae306c0e7f833d3da3c
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-06-17 19:19:34 -0300

    feat: findBy title, director, genre, year, ratingAverage

--- src/main/java/ifsp/movietex/usuario/Usuario.java
+++ src/main/java/ifsp/movietex/usuario/Usuario.java
@@ -7,7 +7,7 @@ public class Usuario {
 	private String senha;
 	private boolean admin;
 	
-	public Usuario(String nome, String email, String senha) 
+	public Usuario(String nome, String email, String senha) {
 		this.nome = nome;
 		this.email = email;
 		this.senha = senha;
