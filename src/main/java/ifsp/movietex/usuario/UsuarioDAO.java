7cac25b853b0155768ee8ec24c1e2bf582f88106
Amanda <amandacapistrano@outlook.com>
2024-06-17 19:05:36 -0300

    rascunho de login e cad

---
+++ src/main/java/ifsp/movietex/usuario/UsuarioDAO.java
@@ -0,0 +1,14 @@
+package ifsp.movietex.usuario;
+
+public class UsuarioDAO {
+
+	public void cadastrar(Usuario usuario) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Usuario findByEmail(String email) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
