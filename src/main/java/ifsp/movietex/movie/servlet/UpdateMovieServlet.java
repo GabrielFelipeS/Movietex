09473b5d6f63d6c0d7aaa9711ac9e8942015459f
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-06-19 18:52:46 -0300

    feat: FindMovieServlet, UpdateMovieServlet, DeleteMovieServlet

---
+++ src/main/java/ifsp/movietex/movie/servlet/UpdateMovieServlet.java
@@ -0,0 +1,63 @@
+package ifsp.movietex.movie.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+import ifsp.movietex.base.db.ConnectionPostgress;
+import ifsp.movietex.base.db.ResponseWrapper;
+import ifsp.movietex.movie.dao.MovieDAO;
+import ifsp.movietex.movie.entity.DTOMovie;
+
+/**
+ * Servlet implementation class UpdateMovieServlet
+ */
+@WebServlet("/api/movie/update")
+public class UpdateMovieServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("application/json;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+
+		String title = request.getParameter("title");
+		String description = request.getParameter("description");
+		String director = request.getParameter("director");
+		String genre = request.getParameter("genre");
+
+		String yearStr = request.getParameter("year");
+		Integer year = yearStr != null ? Integer.valueOf(yearStr) : null;
+
+		String minRatingAverageStr = request.getParameter("minRatingAverage");
+		Double minRatingAverage = minRatingAverageStr != null ? Double.valueOf(minRatingAverageStr) : null;
+
+		String maxRatingAverageStr = request.getParameter("maxRatingAverage");
+		Double maxRatingAverage = maxRatingAverageStr != null ? Double.valueOf(maxRatingAverageStr) : null;
+
+		Connection conn = new ConnectionPostgress().getConnection();
+		MovieDAO dao = new MovieDAO(conn);
+		String msg = dao.insert(new DTOMovie(title, description, genre, director, year));
+
+		Gson gson = new Gson();
+
+		ResponseWrapper wrapper = new ResponseWrapper();
+		
+		wrapper.setStatus(msg.contains("Sucesso") ? HttpServletResponse.SC_OK : HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+		wrapper.setData(msg);
+
+		String json = gson.toJson(wrapper);
+
+		out.print(json);
+		out.flush();
+
+	}
+
+}
