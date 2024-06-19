6f36a0a980136149efc0d700d38437507ece62eb
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-06-19 18:20:26 -0300

    feat: AdvancedSearch, SimpleSearch and InputMovieServlet

--- src/main/java/ifsp/movietex/movie/servlet/AdvancedSearchServlet.java
+++ src/main/java/ifsp/movietex/movie/servlet/AdvancedSearchServlet.java
@@ -1,6 +1,7 @@
 package ifsp.movietex.movie.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.util.List;
 
@@ -9,45 +10,52 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import com.google.gson.Gson;
 
 import ifsp.movietex.base.db.ConnectionPostgress;
+import ifsp.movietex.base.db.ResponseWrapper;
 import ifsp.movietex.movie.dao.MovieDAO;
 import ifsp.movietex.movie.entity.Movie;
 
-@WebServlet("/movie")
-public class GetAllMoviesServlet extends HttpServlet {
+@WebServlet("/api/movie/advancedSearch")
+public class AdvancedSearchServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session = request.getSession();
-		
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("application/json;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+
 		String title = request.getParameter("title");
 		String description = request.getParameter("description");
 		String director = request.getParameter("director");
 		String genre = request.getParameter("genre");
-		
+
 		String yearStr = request.getParameter("year");
-		Integer year = yearStr != null ? Integer.valueOf(yearStr): null;
-		
+		Integer year = yearStr != null ? Integer.valueOf(yearStr) : null;
+
 		String minRatingAverageStr = request.getParameter("minRatingAverage");
-		Double minRatingAverage = minRatingAverageStr != null ? Double.valueOf(minRatingAverageStr): null;
-		
+		Double minRatingAverage = minRatingAverageStr != null ? Double.valueOf(minRatingAverageStr) : null;
+
 		String maxRatingAverageStr = request.getParameter("maxRatingAverage");
-		Double maxRatingAverage = maxRatingAverageStr != null ? Double.valueOf(maxRatingAverageStr): null;
-		
+		Double maxRatingAverage = maxRatingAverageStr != null ? Double.valueOf(maxRatingAverageStr) : null;
+
 		Connection conn = new ConnectionPostgress().getConnection();
 		MovieDAO dao = new MovieDAO(conn);
 		List<Movie> movies = dao.findBy(title, description, genre, director, year, minRatingAverage, maxRatingAverage);
-		
+
 		movies.forEach(System.out::println);
-		
+
 		Gson gson = new Gson();
-		String json = gson.toJson(movies);
-		System.out.println(json);
-		
-		session.setAttribute("result", json);
+
+		ResponseWrapper wrapper = new ResponseWrapper();
+		wrapper.setStatus(HttpServletResponse.SC_OK);
+		wrapper.setData(movies);
+
+		String json = gson.toJson(wrapper);
+
+		out.print(json);
+		out.flush();
 	}
 }
