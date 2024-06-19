b9f02bba35b318792c21f6d3e7cda41cb6e536d4
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-06-18 23:16:49 -0300

    refactor: movie

---
+++ src/main/java/ifsp/movietex/movie/MovieServlet.java
@@ -0,0 +1,24 @@
+package ifsp.movietex.movie;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/MovieServlet")
+public class MovieServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getSession().setAttribute("MICAEL", "fashkfhajf");
+
+		doGet(request, response);
+	}
+
+}
--- src/main/java/ifsp/movietex/movie/entity/Movie.java
+++ src/main/java/ifsp/movietex/movie/entity/Movie.java
@@ -3,14 +3,16 @@ package ifsp.movietex.movie.entity;
 public class Movie {
 	private Integer id;
 	private String title;
+	private String description;
 	private String director;
 	private String genre;
 	private Integer year;
 	private Double ratingAverage;
 
-	public Movie(Integer id, String title, String director, String genre, Integer year, Double ratingAverage) {
+	public Movie(Integer id, String title, String description, String director, String genre, Integer year, Double ratingAverage) {
 		this.id = id;
 		this.title = title;
+		this.description = description;
 		this.director = director;
 		this.genre = genre;
 		this.year = year;
