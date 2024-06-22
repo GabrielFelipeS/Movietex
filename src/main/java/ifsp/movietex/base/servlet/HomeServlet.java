6d1b424658ad64b40b347456b20e825d77215b15
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-06-22 20:13:42 -0300

    fix: Tomcat

---
+++ src/main/java/ifsp/movietex/base/servlet/HomeServlet.java
@@ -0,0 +1,20 @@
+package ifsp.movietex.base.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HomeServlet
+ */
+@WebServlet("/home")
+public class HomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/index.jsp").forward(request, response);;
+	}
+}
