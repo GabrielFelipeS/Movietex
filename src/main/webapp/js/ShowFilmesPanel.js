f418caa2d78ec9ac64576a90b69c12e86253051f
Micael Hernandes <micael.hernandes>
2024-06-24 20:00:24 -0300

    feat: filmes no painel

--- src/main/webapp/js/ShowFilmesPanel.js
+++ src/main/webapp/js/ShowFilmesPanel.js
@@ -3,25 +3,31 @@ const PainelFilmsArea = document.querySelector('#PainelFilmsArea');
 console.log(PainelFilmsArea);
 
 window.addEventListener('load', async () => {
-    let url = window.baseUrl + '/api/movies/findall';
+    const currentUrl = window.location.href;
+
+    let baseUrl = currentUrl.replace(/\/painel.jsp\/?$/, '');
+
+    let url = baseUrl+ '/api/movie/findAll';
+    console.log(url)
     const response = await fetch(url);
     const data = await response.json();
+    console.log(data);
     data.forEach(filme => {
         PainelFilmsArea.innerHTML += `
         <div
                     class="w-[90%] h-[150px] bg-primary text-white rounded-lg flex flex-row gap-10 justify-between items-center">
                 <div class="flex flex-row gap-5 h-full items-center ml-2">
-                    <img src="img/capas/divertida_mente.webp" alt="Cartaz do Filme X" class="h-[80%] rounded-lg">
+                    <img src="${filme.poster}" alt="Cartaz do Filme X" class="h-[80%] rounded-lg">
                     <div>
-                        <h2 class="text-2xl">Filme X</h2>
-                        <p><span class="font-bold"><a href="">Aventura</a></span></p>
-                        <p>Direção: <span class="font-bold"><a href="">Kelsey Mann</a></span></p>
-                        <p>Ano Lançamento: <span class="font-bold">2024</span></p>
+                        <h2 class="text-2xl">${filme.title}</h2>
+                        <p><span class="font-bold"><a href="">${filme.genre}</a></span></p>
+                        <p>Direção: <span class="font-bold"><a href="">${filme.director}</a></span></p>
+                        <p>Ano Lançamento: <span class="font-bold">${filme.year}</span></p>
                     </div>
                 </div>
                 <div class="flex items-center p-5 gap-5">
-                    <a href="" class="hover:-translate-y-1 transition-all duration-150"><i class="fa-solid fa-pen-to-square text-2xl text-white"></i></a>
-                    <a href="" class="hover:-translate-y-1 transition-all duration-150"><i class="fa-solid fa-trash-can text-2xl text-white"></i></a>
+                    <a href="${baseUrl+'/painel/edit/'+filme.id}" class="hover:-translate-y-1 transition-all duration-150"><i class="fa-solid fa-pen-to-square text-2xl text-white"></i></a>
+                    <a href="${baseUrl+'/painel/delete/'+filme.id}" class="hover:-translate-y-1 transition-all duration-150"><i class="fa-solid fa-trash-can text-2xl text-white"></i></a>
                 </div>
             </div>`
     })
