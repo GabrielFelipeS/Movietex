9a55e8a7385e747bc45cafb5e98e8eb42b68859f
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2024-06-25 18:37:24 -0300

    new endpoint cadastro

--- src/main/java/ifsp/movietex/movie/servlet/views/CadastroMovie.java
+++ src/main/java/ifsp/movietex/movie/servlet/views/CadastroMovie.java
@@ -14,7 +14,7 @@ public class CadastroMovie extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	    RequestDispatcher dispatcher = request.getRequestDispatcher("/casttro.jsp");
+	    RequestDispatcher dispatcher = request.getRequestDispatcher("/castro.jsp");
         dispatcher.forward(request, response);
 	}
 
